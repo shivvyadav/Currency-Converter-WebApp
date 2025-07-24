b1570d44936c6da578dc96e725082545c61f41f2
Shiv Yadav <shivyadav0014@gmail.com>
2025-07-24 22:37:06 +0545

    finally complete js code

--- app.js
+++ app.js
@@ -51,3 +51,12 @@ const updateFlag = (element) => {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
